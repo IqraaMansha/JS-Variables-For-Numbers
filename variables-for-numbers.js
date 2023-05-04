fa7ecf791c2c44b87e97d924b563913c6792c451
Iqra Mansha <iqramansha78u@gmail.com>
2023-05-04 13:03:09 +0500

    First Commit

---
+++ variables-for-numbers.js
@@ -0,0 +1,24 @@
+// #3 VARIABLES FOR NUMBERS
+
+// price = 250;
+// Discount = 50;
+// document.write(price - Discount);
+
+console.log("#3 VARIABLES FOR NUMBERS");
+
+var age = "I am 16 Years old";
+alert(age);
+
+var visitor = 14;
+alert(" You have visited this site " + 14 + " times ");
+
+var birthyear = 1;
+document.write('What is Your birthyear ? <br> ' + ' <br> My Birth Year is 2006 <br> <br> ');
+
+var name = " Iqra Mansha ";
+var quantity = "6";
+var product = "Shirts";
+document.write(name + " Ordered " + quantity + " " + product + " On XYZ Cloth Store ");
+
+
+
